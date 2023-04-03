4c31fa029294df12595980ca77feeae0b3290949
Levi Evans <levi.samuel.evans@cern.ch>
2023-04-03 23:41:52 +0200

    Upgrading script to suit style of hupdate command

--- HTCondor/mergeHistos.py
+++ HTCondor/mergeHistos.py
@@ -2,26 +2,30 @@ import os
 import subprocess
 
 # Define the command and filenames
-executable = "/scratch4/levans/TRExFitter_v4.17/TRExFitter/build/bin/hupdate.exe"
+executable2 = "/scratch4/levans/TRExFitter_v4.17/TRExFitter/build/bin/hupdate.exe"
 input_file = "Fit_1l_ttH_STXS2_1l_histos.root"
-base_output_file = "Fit_1l_ttH_STXS2_1l_histos_Zjets_XS.root"
+base_output_file = "Fit_1l_ttH_STXS2_1l_histos_{}.root"
 
 # Read the list of systematics from a text file
 with open("syst.txt", "r") as f:
     systematics = [line.strip() for line in f.readlines()]
 
-# Loop through the list of systematics and run the command with the modified filenames
+# Initialize the list of output files
+output_files = []
+
+# Loop through the list of systematics and generate the output filenames
 for systematic in systematics:
-    # Replace "Zjets_XS" with the systematic
-    output_file = base_output_file.replace("Zjets_XS", systematic)
+    # Format the output filename with the systematic
+    output_file = base_output_file.format(systematic)
 
-    # Ensure the replacement was successful, if not, print an error message and skip the iteration
-    if output_file == base_output_file:
-        print(f"Failed to replace 'Zjets_XS' with '{systematic}' in the output filename.")
-        continue
+    # Append the output file to the list of output files
+    output_files.append(output_file)
 
-    # Build the command
-    cmd = f"{executable} {input_file} {output_file}"
+# Join the list of output files as a single string
+output_files_str = ' '.join(output_files)
 
-    # Run the command in the terminal
-    subprocess.run(cmd, shell=True, check=True)
+# Build the hupdate command
+hupdate_cmd = f"{executable2} {input_file} {output_files_str}"
+
+# Run the hupdate command in the terminal
+subprocess.run(hupdate_cmd, shell=True, check=True)
