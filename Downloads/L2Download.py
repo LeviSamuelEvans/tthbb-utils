8a2a8729b0da778faec68daf93552836c34cda2e
Levi Evans <levi.samuel.evans@cern.ch>
2023-08-29 23:30:03 +0200

    feat: adding corr diff script and large overhaul of structure/tidy-up

--- Downloads/L2Download.py
+++ Downloads/L2Download.py
@@ -1,87 +1,91 @@
 #!/usr/bin/env python3
-"""
-A script to assist in downloading the ttH(bb) Legacy Analysis Common L2 Ntuples from the Higgs group disk to linappserv LE v0.1 17.03.23
 
 """
+A script to assist in downloading the ttH(bb) Legacy Analysis Common L2 Ntuples
+from the Higgs group disk to linappserv.
 
-##############
-# USAGE TIPS #
-##############
+Run the script with 'L2Download.py', then ctrl-z to put the process to sleep, then
+bg to have it run in the background
 
-"""
-Make sure you are using python v3.6 or onwards for this script, it does not work with python2
-
-Run the script with 'python L2Download.py', then ctrl-z to put the process to sleep, then bg to have it run in the background
-
-If you are running on an ssh connection, you can also disown the process so that it still runs if you get disconnected.
-i.e run jobs -> get job number -> then 'disown %[JobId]'
+If you are running on an ssh connection, you can also disown the process so that it
+still runs if you get disconnected.
+i.e run jobs -> get job number -> then 'disown %[JobId]' (not ideal)
 Alternatively, use screen or nohup.
 
-"""
-
-
-
-import os
-import subprocess
-from concurrent.futures import ThreadPoolExecutor
-
-# Define the directories to download
-directories = ['1l/5j3b_ttb/','1l/5j3b_ttbb/','1l/5j3b_ttc/','1l/5j3b_ttlight/','1l/5j3b_ttH/','1l/5j3b_ttB/','1l/boosted/', '2l/3j3b_ttb/','2l/3j3b_ttbb/','2l/3j3b_ttB/','2l/3j3b_ttc/','2l/3j3b_ttlight/','2l/3j3b_ttH/','2l/boosted/']
-
-# Define the rsync command with options
-'''
 Options for rsync
 -v: Verbose output, print information about each file as it is transferred
 -r: Recursively copy entire directories
 -a: Both recursively copy directories and copy file read/write permissions and ownership information
--z: Compress the data during transfer to reduce network traffic [HIGHLY RECOMMENDED]
--P: Show progress information and continue partial transfers if the transfer is interrupted [Needed for log files and useful for interuptions]
--n: perform a dry-run of the download to check all is working as should [RECOMMENDED TO DO BEFORE ACTUAL DOWNLOAD!]
--e ssh: This will tell rsync to use ssh for authenticaion. You can set up a ssh-key such that you won't have to enter a password. Info in the README.md about how to do this :)
+-z: Compress the data during transfer to reduce network traffic
+-P: Show progress information and continue partial transfers if the transfer is interrupted
+    [Needed for log files and useful for interuptions]
+-n: perform a dry-run of the download to check all is working as should
+    [RECOMMENDED TO DO BEFORE ACTUAL DOWNLOAD!]
+-e ssh: This will tell rsync to use ssh for authenticaion. You can set up a ssh-key such that
+        you won't have to enter a password. Info in the README.md about how to do this :)
 -- progress : This will give an estimate of the time remaining for the download
+-- delete : This will delete any files in the destination directory that are not present
+            in the source directory. This is useful if you want to update a directory with new files.
+-- exclude : This will exclude any files that match the pattern given. This is useful if you want to
+             download only a subset of files in a directory.
+"""
 
-'''
+def download_directory(directory, log_file):
+    """
+    Download a directory of files using rsync.
+    """
+    os.makedirs(directory, exist_ok=True)
+    source = f"leevans@lxplus.cern.ch:/eos/atlas/atlascerngroupdisk/phys-higgs/HSG8/ttHbb_legacy_L2ntuples/sys_v2.1/{directory}"
+    destination = f"./{directory}"
+    rsync_args = ["rsync", "-vrznPe ssh", source, destination]
 
-rsync_command = ["rsync", "-vrznPe ssh"] # rsync command followed by actions
+    result = subprocess.run(" ".join(rsync_args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
-# Open an overall log file for the Python script
-with open('FullProduction.log', 'w') as log_file:
+    # Write individual log files for each directory
+    with open(os.path.join(directory, 'rsync.log'), 'w') as f:
+        f.write(result.stdout.decode())
+        f.write(result.stderr.decode())
 
-    # Define the function to download a directory of files
-    def download_directory(directory):
-        os.makedirs(directory, exist_ok=True)
-        source = "leevans@lxplus.cern.ch:/eos/atlas/atlascerngroupdisk/phys-higgs/HSG8/ttHbb_legacy_L2ntuples/sys_v2.1/" + directory
-        destination = "./" + directory # the "./" here specifies the cwd, but you can add any file path you want here.
-        rsync_args = rsync_command + [source, destination]
-        result = subprocess.run(" ".join(rsync_args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        # Write log files for each individual directory being downloaded
-        with open(os.path.join(directory, 'rsync.log'), 'w') as f:
-            f.write(result.stdout.decode())
-            f.write(result.stderr.decode())
-        # Write the output of each rsync command to the overall log file
-        log_file.write(f"Output for {directory}:\n")
-        log_file.write(result.stdout.decode())
-        log_file.write(result.stderr.decode())
+    # Write output to the overall log file
+    log_file.write(f"Output for {directory}:\n")
+    log_file.write(result.stdout.decode())
+    log_file.write(result.stderr.decode())
 
-    # Execute the rsync commands in parallel using a thread pool
-    with ThreadPoolExecutor(max_workers=14) as executor:
-        futures = [executor.submit(download_directory, directory) for directory in directories]
+def main():
+    """
+    Main function to execute the script.
+    """
+    directories = [
+        '1l/5j3b_ttb/', '1l/5j3b_ttbb/', '1l/5j3b_ttc/', '1l/5j3b_ttlight/',
+        '1l/5j3b_ttH/', '1l/5j3b_ttB/', '1l/boosted/', '2l/3j3b_ttb/',
+        '2l/3j3b_ttbb/', '2l/3j3b_ttB/', '2l/3j3b_ttc/', '2l/3j3b_ttlight/',
+        '2l/3j3b_ttH/', '2l/boosted_STXS5/', '2l/boosted_STXS6/'
+    ]
 
-    # Wait for all the downloads to finish
-    for future in futures:
-        future.result()
+    # Open an overall log file for the Python script
+    with open('FullProduction.log', 'w') as log_file:
+        # Execute the rsync commands in parallel using a thread pool
+        with ThreadPoolExecutor(max_workers=14) as executor:
+            futures = [executor.submit(download_directory, directory, log_file) for directory in directories]
 
+        # Wait for all the downloads to finish
+        for future in futures:
+            future.result()
 
+if __name__ == "__main__":
+    main()
+
+"""
 ################################
 # A Note on using thread pool  #
 ################################
 
-"""
-The max_workers parameter of the ThreadPoolExecutor constructor specifies the maximum number of worker threads to use for executing the submitted tasks.
-In this case, the value of max_workers is set to 14, which means that the executor will use up to 14 threads to execute the download_directory function in parallel.
+The max_workers parameter of the ThreadPoolExecutor constructor specifies the maximum number of worker threads to use for
+executing the submitted tasks. In this case, the value of max_workers is set to 14, which means that the executor will use
+up to 14 threads to execute the download_directory function in parallel.
 
-The ThreadPoolExecutor creates a pool of worker threads that can execute multiple tasks concurrently, which can improve the performance of the script by allowing it to
-download multiple directories simultaneously. The number of worker threads should be chosen based on the number of available CPU cores and the expected workload of the script,
-to ensure that the script does not overwhelm the system or create a bottleneck. Run 'lscpu' to get more information on the specs of the machine you are using.
-
+The ThreadPoolExecutor creates a pool of worker threads that can execute multiple tasks concurrently, which can improve the
+performance of the script by allowing it to download multiple directories simultaneously. The number of worker threads should
+be chosen based on the number of available CPU cores and the expected workload of the script, to ensure that the script does
+not overwhelm the system or create a bottleneck. Run 'lscpu' to get more information on the specs of the machine you are using.
 """
--- Utils/fit-study-scripts/post-fit-acceptance.py
+++ Utils/fit-study-scripts/post-fit-acceptance.py
@@ -3,15 +3,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from atlasify import atlasify
-
+"""
 # order in .tex is ttb,tB,2b,c,light
 
-## Quick Plot to examine post-fit accectance effects of Nuisance Parameters
+Quick Plot to examine post-fit accectance effects of Nuisance Parameters
 
-# -- Improve code by reading straight from .tex files and plot all together 
+TODO:
+    - Improve code by reading straight from .tex files and plot all together
+    - implement a function for this
+    - move to mlphep for ATLAS plotting styles
+    - Normalise to total effect per region
+    - improve plot aesthetics
 
-# --------------------------------------------------------------------------------------
-
+"""
 #====================#
 # tt+light Component #
 #====================#
@@ -134,13 +138,13 @@ tt_2b_data = {'norm tt1B-4FS-PP8-dip': 3.5,
               'norm tt1B-4FS-scale-var': -2.9,
               'norm ttB-norm': -12,
               'norm ATLAS-FTAG-L0' : 1.7,
-              'norm ATLAS-JES-Modelling' : -1, 
+              'norm ATLAS-JES-Modelling' : -1,
               'norm ATLAS-JES-PU-Rho' : -1.4
               }
 
 ttc_data = {'norm tt1B-4FS-PH7-PS': -2.5,
             'norm tt1B-4FS-scale-var': -2.1,
-            'norm ttB-norm' : -12 
+            'norm ttB-norm' : -12
             }
 
 ttB_data = {'norm tt1B-4FS-PH7-PS': -2.5,
@@ -165,7 +169,7 @@ tt_2b_data = {'norm ttc-dropc-PS-QUAD': 1.1,
               'norm ttc-pthard1-gen': -1.4,
               'norm ttc-dropc-FSR-QUAD': 1.7,
               'norm ATLAS-FTAG-L0' : 2,
-              'norm ATLAS-JER-EffectiveNP' : 1.1, 
+              'norm ATLAS-JER-EffectiveNP' : 1.1,
               'norm ATLAS-JES-Modelling' : -1.4,
               'ATLAS-JES-PU-Rho' : -1.9
               }
@@ -174,7 +178,7 @@ ttc_data = {'norm bTag-High-pT-extrap1-b': 1.6,
             'norm bTag-High-pT-extrap1-c': 1.5,
             'norm bTag-High-pT-extrap1-light' : 1,
             'norm ttc-pthard1-gen' : -1.3,
-            'norm ATLAS-FTAG-L0' : 1.3, 
+            'norm ATLAS-FTAG-L0' : 1.3,
             'norm ATLAS-JES-PU-Rho' : -1.4
             }
 
@@ -183,7 +187,7 @@ ttB_data = {'nnorm bTag-High-pT-extrap1-b': 4.9,
             'norm bTag-High-pT-extrap1-light' : 3.1,
             'ttc-dropc-PS-QUAD' : 1.9,
             'ttc-dropc-FSR-QUAD' : 1.3,
-            'ATLAS-FTAG-C1' : 1.5 
+            'ATLAS-FTAG-C1' : 1.5
             }
 
 # --------------------------------------------------------------------------------------
@@ -205,7 +209,7 @@ ax.tick_params(axis='both', labelsize=6)
 ax.grid(axis='y', linestyle='--', alpha=0.7)
 
 
-# Combine data into one dataframe for plotting the bar graph 
+# Combine data into one dataframe for plotting the bar graph
 df = pd.concat([tt_light_df, tt_1b_df, tt_2b_df,ttc_df, ttB_df,], axis=0)
 df['Region'] = ['tt+light'] * len(tt_light_df) + ['tt+1b'] * len(tt_1b_df) + \
                ['tt+≥2b'] * len(tt_2b_df) + ['tt+≥1c'] * len(ttc_df) + ['tt+B'] * len(ttB_df)
@@ -222,7 +226,7 @@ colors = {
 # Convert RGB tuple to hex code
 colors = {key: '#{:02x}{:02x}{:02x}'.format(*value) for key, value in colors.items()}
 
-# Create bar plot using Seaborn 
+# Create bar plot using Seaborn
 sns.barplot(x='Parameter', y='Effect', hue='Region', data=df, ax=ax, palette=colors, linewidth=15, saturation=1.0, width=0.85)
 
 # Rotate x-axis labels
@@ -230,13 +234,13 @@ plt.xticks(rotation=45, ha='right')
 
 ax.axhline(y=0, color='black', linewidth=0.5)
 
-# Add legend outside the plot for each region 
+# Add legend outside the plot for each region
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 # Adjust plot margins
 plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9, wspace=0.2, hspace=0.2)
 
-# Set y-axis tick frequency 
+# Set y-axis tick frequency
 ax.yaxis.set_major_locator(ticker.MultipleLocator(1.0))
 ax.set_xlabel('Nuisance parameter')
 ax.set_ylabel('Acceptance effect (%)')
